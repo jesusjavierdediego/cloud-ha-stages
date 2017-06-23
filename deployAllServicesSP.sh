0d384cb2d3bf9a053de871f0ade0829f9eaa1eb3
fmunoz <fmuno@fexco.com>
2017-06-23 15:05:58 +0200

    All services working in HA with enhanced timing in healthcheck

---
+++ deployAllServicesSP.sh
@@ -0,0 +1,4 @@
+./deployService.sh qatsp fxms spfxms 2 8.2.270-SNAPSHOT 8085 10085 1008
+./deployService.sh qatsp fxpos spfxpos 2 8.2.270-SNAPSHOT 8090 10090 1009
+./deployService.sh qatsp notificationserver notificationserver 2 0.5.40-SNAPSHOT 10005 1050 1005
+./deployService.sh qatsp bureaumobileweb bmwsp 2 0.1.99-SNAPSHOT 10004 1040 1004
